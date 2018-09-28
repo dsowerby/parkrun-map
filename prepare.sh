532b180831ff1022ba2d0f5234d5796c83d00f73
Dave Sowerby-Williams <dave.sowerby-williams@propagatorgroup.com>
2018-09-28 10:29:52 +0100

    Updating prepare script to cachebust, as well as uglify the resources ready to be served.

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,9 @@
 #wget https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml -O docs/geo.xml
 #wget https://www.cssscript.com/demo/basic-hamburger-toggle-menu-css-vanilla-javascript/styles/nav.css -O docs/hamburger.css
-minify map.js > docs/map.min.js
-minify map.css > docs/map.min.css
-minify hamburger.css > docs/hamburger.min.css
+# npm install uglify-js -g
+uglifyjs map.js > docs/map.min.js
+# npm install uglifycss -g
+uglifycss map.css > docs/map.min.css
+uglifycss hamburger.css > docs/hamburger.min.css
+CACHEBUST=`date +%s`
+sed 's/CACHEBUST/'$CACHEBUST'/g' index.html > docs/index.html
