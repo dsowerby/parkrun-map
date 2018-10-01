274e1fb7c53eebbef59a9008486b90e2365a9f76
Dave Sowerby-Williams <dave.sowerby-williams@propagatorgroup.com>
2018-10-01 12:52:46 +0100

    Adding in a vegan version, to include happycow links.

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,7 @@
 #wget https://www.cssscript.com/demo/basic-hamburger-toggle-menu-css-vanilla-javascript/styles/nav.css -O docs/hamburger.css
 #sudo npm install uglify-js -g
 uglifyjs map.js > docs/map.min.js
+uglifyjs map.vegan.js > docs/vegan/map.min.js
 #sudo npm install uglifycss -g
 uglifycss map.css > docs/map.min.css
 uglifycss hamburger.css > docs/hamburger.min.css
