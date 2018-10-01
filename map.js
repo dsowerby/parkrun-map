290959fcbd10e598aca15f2d75f52240a7430599
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2018-10-01 18:19:26 +0100

    Forced rebuild.

--- map.js
+++ map.js
@@ -84,7 +84,7 @@ function displayEvents(include) {
 			if (typeof(elementRegionUrl) !== undefined) {
 				var markerContent = '<strong>'+ name + '</strong><br /><a target="_blank" href="' + elementRegionUrl + '/' + elementId + '">Course page</a><br /><a target="_blank" href="https://www.google.com/maps/dir/?api=1&destination='+latitude+',' + longitude + '">Directions</a>';
 				if (options.vegan) {
-					markerContent += '<br /><a target="_blank" href="https://www.happycow.net/searchmap?lat=51&lng=1&vegan=true">Local vegan food</a>';
+					markerContent += '<br /><a target="_blank" href="https://www.happycow.net/searchmap?lat='+latitude+'&lng='+longitude+'&vegan=true">Local vegan food</a>';
 				}
 				marker.bindPopup(markerContent);
 			} else {
