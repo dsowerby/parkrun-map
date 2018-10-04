cf5c81a61492a74a6edc3e45b71ac2b8bf670f92
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2018-10-04 07:55:50 +0100

    Updating geo.

--- fetch-geo.sh
+++ fetch-geo.sh
@@ -1,1 +1,1 @@
-wget https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml -O docs/geo.xml
+wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml -O docs/geo.xml
