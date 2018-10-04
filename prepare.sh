5dceb78f6a65e63288cd4274d6d85554452303ff
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2018-10-04 15:21:46 +0100

    Compress/Rename on the JS.

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #wget https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml -O docs/geo.xml
 #wget https://www.cssscript.com/demo/basic-hamburger-toggle-menu-css-vanilla-javascript/styles/nav.css -O docs/hamburger.css
 #sudo npm install uglify-js -g
-uglifyjs map.js > docs/map.min.js
+uglifyjs --rename map.js > docs/map.min.js
 #sudo npm install uglifycss -g
 uglifycss map.css > docs/map.min.css
 uglifycss hamburger.css > docs/hamburger.min.css
