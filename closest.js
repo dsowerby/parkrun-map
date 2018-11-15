791af30146ad9dc171bddbac315bf66a7a67931d
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2018-11-15 20:19:24 +0000

    Yet more correction to marker.

--- closest.js
+++ closest.js
@@ -150,6 +150,14 @@ function displayEvents(closest) {
 		});
 	});
 
+	var markerIcon = L.icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+		shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	});
 	var marker = L.marker([closestLatitude, closestLongitude], { icon: markerIcon});
 	marker.addTo(markerGroup);
 
