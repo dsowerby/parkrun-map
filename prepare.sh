3cafdbea427a481baa0c7d4bbeb907528011fc31
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2019-10-28 19:19:05 +0000

    Adding in IE NYD

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,7 @@
 #sudo npm install uglify-js -g
 uglifyjs --rename map.js > docs/map.min.js
 uglifyjs --rename map-nyd.js > docs/nyd/map.min.js
+uglifyjs --rename map-nyd-ie.js > docs/nyd/ie/map.min.js
 uglifyjs --rename map-juniors.js > docs/juniors/map.min.js
 uglifyjs --rename closest.js > docs/closest/closest.min.js
 uglifyjs --rename alphabeteer.js > docs/alphabeteer/alphabeteer.min.js
@@ -11,6 +12,7 @@ uglifyjs --rename alphabeteer.js > docs/alphabeteer/alphabeteer.min.js
 uglifycss map.css > docs/map.min.css
 uglifycss map.css > docs/juniors/map.min.css
 uglifycss map.css > docs/nyd/map.min.css
+uglifycss map.css > docs/nyd/ie/map.min.css
 uglifycss hamburger.css > docs/hamburger.min.css
 uglifycss hamburger.css > docs/juniors/hamburger.min.css
 CACHEBUST=`date +%s`
@@ -19,3 +21,4 @@ sed 's/CACHEBUST/'$CACHEBUST'/g' index-closest.html > docs/closest/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index-alphabeteer.html > docs/alphabeteer/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index.html > docs/juniors/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index-nyd.html > docs/nyd/index.html
+sed 's/CACHEBUST/'$CACHEBUST'/g' index-nyd.html > docs/nyd/ie/index.html
