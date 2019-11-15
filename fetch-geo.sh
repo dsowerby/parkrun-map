b0b0001c554b37a6fb1a8e77b29545871c3dde68
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2019-11-15 20:22:02 +0000

    First upload of events, preventing future download of old geo data

--- fetch-geo.sh
+++ fetch-geo.sh
@@ -1,2 +1,3 @@
-wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml -O docs/geo.xml
-wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geojuniors.xml -O docs/juniors/geojuniors.xml
+#wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml -O docs/geo.xml
+#wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geojuniors.xml -O docs/juniors/geojuniors.xml
+wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://images.parkrun.com/events.json -O docs/events.json
