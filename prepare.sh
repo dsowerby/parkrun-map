19ab31e01368b9cde51598a3e077e32d557662c8
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2019-12-13 12:21:28 +0000

    Adding in an XMAS map

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,7 @@
 uglifyjs --rename map.js > docs/map.min.js
 uglifyjs --rename map-nyd.js > docs/nyd/map.min.js
 uglifyjs --rename map-nyd-ie.js > docs/nyd/ie/map.min.js
+uglifyjs --rename map-xmas.js > docs/xmas/map.min.js
 uglifyjs --rename map-juniors.js > docs/juniors/map.min.js
 uglifyjs --rename closest.js > docs/closest/closest.min.js
 uglifyjs --rename alphabeteer.js > docs/alphabeteer/alphabeteer.min.js
@@ -13,6 +14,7 @@ uglifycss map.css > docs/map.min.css
 uglifycss map.css > docs/juniors/map.min.css
 uglifycss map.css > docs/nyd/map.min.css
 uglifycss map.css > docs/nyd/ie/map.min.css
+uglifycss map.css > docs/xmas/map.min.css
 uglifycss hamburger.css > docs/hamburger.min.css
 uglifycss hamburger.css > docs/juniors/hamburger.min.css
 CACHEBUST=`date +%s`
@@ -21,4 +23,5 @@ sed 's/CACHEBUST/'$CACHEBUST'/g' index-closest.html > docs/closest/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index-alphabeteer.html > docs/alphabeteer/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index.html > docs/juniors/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index-nyd.html > docs/nyd/index.html
+sed 's/CACHEBUST/'$CACHEBUST'/g' index-xmas.html > docs/xmas/index.html
 sed 's/CACHEBUST/'$CACHEBUST'/g' index-nyd.html > docs/nyd/ie/index.html
