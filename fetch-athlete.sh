ab7a565bfc00ee17f6f64acf6b6c2d388146db29
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2022-01-11 09:28:11 +0000

    Adding athlete 3347093
    Also making the script compatible with linux and mac

--- fetch-athlete.sh
+++ fetch-athlete.sh
@@ -1,4 +1,4 @@
-read -r -a ATHLETE_ID -p "Enter athlete ID: "
+read -r -p "Enter athlete ID: " ATHLETE_ID
 wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
 egrep -o 'https://[^/]+/([^/]+)/results/' docs/athletes/$ATHLETE_ID.html | sort | uniq | awk -F '/' '{ print $4 }' | jq -R '[.]' | jq -s -c 'add' > docs/athletes/$ATHLETE_ID.json
 rm docs/athletes/$ATHLETE_ID.html
