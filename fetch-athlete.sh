dd820ed46fcc946ec41e77e2fff22c2c6b004462
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2022-01-25 06:04:03 +0000

    Making fetch-athlete accept cli arg, adding fetch all (needs work) and updating

--- fetch-athlete.sh
+++ fetch-athlete.sh
@@ -1,4 +1,8 @@
-read -r -p "Enter athlete ID: " ATHLETE_ID
+if [ $# -eq 0 ]; then
+    read -r -p "Enter athlete ID: " ATHLETE_ID
+else
+    ATHLETE_ID=$1
+fi
 wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
 egrep -o 'https://[^/]+/([^/]+)/results/' docs/athletes/$ATHLETE_ID.html | sort | uniq | awk -F '/' '{ print $4 }' | jq -R '[.]' | jq -s -c 'add' > docs/athletes/$ATHLETE_ID.json
 rm docs/athletes/$ATHLETE_ID.html
---
+++ fetch-athletes.sh
@@ -0,0 +1,8 @@
+SOURCE_DIR=$(dirname "$0")
+
+$SOURCE_DIR/fetch-athlete.sh 316947
+$SOURCE_DIR/fetch-athlete.sh 1142363
+$SOURCE_DIR/fetch-athlete.sh 1328433
+$SOURCE_DIR/fetch-athlete.sh 2810853
+$SOURCE_DIR/fetch-athlete.sh 3169399
+$SOURCE_DIR/fetch-athlete.sh 3347093
