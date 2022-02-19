439c801961c3f51384fcf29c5fde715f7b456b7b
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2022-02-19 16:57:51 +0000

    Updating athletes

--- fetch-athlete.sh
+++ fetch-athlete.sh
@@ -4,5 +4,5 @@ else
     ATHLETE_ID=$1
 fi
 wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
-egrep -o 'https://[^/]+/([^/]+)/results/' docs/athletes/$ATHLETE_ID.html | sort | uniq | awk -F '/' '{ print $4 }' | jq -R '[.]' | jq -s -c 'add' > docs/athletes/$ATHLETE_ID.json
+egrep -o 'https://[^/]+/([^/]+)/results/' docs/athletes/$ATHLETE_ID.html | sort | uniq | sort | awk -F '/' '{ print $4 }' | jq -R '[.]' | jq -s -c 'add' > docs/athletes/$ATHLETE_ID.json
 rm docs/athletes/$ATHLETE_ID.html
