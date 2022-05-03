2c7b8921a912d59c2bbb536c53736f5697a68221
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2022-05-03 06:32:31 +0100

    Updating user-agent for fetch athlete

--- fetch-athlete.sh
+++ fetch-athlete.sh
@@ -3,6 +3,6 @@ if [ $# -eq 0 ]; then
 else
     ATHLETE_ID=$1
 fi
-wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
 egrep -o 'https://[^/]+/([^/]+)/results/' docs/athletes/$ATHLETE_ID.html | sort | uniq | sort | awk -F '/' '{ print $4 }' | sort | jq -R '[.]' | jq -s -c 'add' > docs/athletes/$ATHLETE_ID.json
 rm docs/athletes/$ATHLETE_ID.html
