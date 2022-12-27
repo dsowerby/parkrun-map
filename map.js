2342ff7668704852a40572b715b244f42737d252
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2022-12-27 14:31:35 +0000

    Updating facebook and youtube links

--- map.js
+++ map.js
@@ -225,8 +225,8 @@ function addMarker(latitude, longitude, name, iconColour, $event) {
 		markerContent += '<a target="_blank" href="'+eventUrl+'/course/">Course page</a><br />';
 		markerContent += '<a target="_blank" href="'+eventUrl+'/futureroster/">Future Roster</a><br />';
 		markerContent += '<a target="_blank" href="'+eventUrl+'/results/eventhistory/">Event History</a><br />';
-		markerContent += '<a target="_blank" href="https://www.facebook.com/search/top/?q='+ name + '&epa=SEARCH_BOX">Facebook</a><br />';
-		markerContent += '<a target="_blank" href="https://www.youtube.com/results?search_query=' + name + '">YouTube</a><br />';
+		markerContent += '<a target="_blank" href="https://www.facebook.com/search/top/?q='+ name + '%20parkrun&epa=SEARCH_BOX">Facebook</a><br />';
+		markerContent += '<a target="_blank" href="https://www.youtube.com/results?search_query=' + name + '%20parkrun">YouTube</a><br />';
 	} else if (typeof(name) !== 'undefined') {
 		markerContent = name;
 	}
