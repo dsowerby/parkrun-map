7df378660b62792aada25d42d9e8a55680210481
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2024-08-04 12:11:08 +0100

    Updating athlete files
    Also updating the approach for how these are updated

--- fetch-athlete.sh
+++ fetch-athlete.sh
@@ -3,6 +3,13 @@ if [ $# -eq 0 ]; then
 else
     ATHLETE_ID=$1
 fi
-wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
+
+echo Go fetch https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ to docs/athletes/$ATHLETE_ID.html
+code docs/athletes/$ATHLETE_ID.html
+open https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/
+echo "Tell me when that's done"
+read -p "Press Enter to continue..."
+
+#wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
 egrep -o 'https://[^/]+/([^/]+)/results/' docs/athletes/$ATHLETE_ID.html | sort | uniq | sort | awk -F '/' '{ print $4 }' | sort | jq -R '[.]' | jq -s -c 'add' > docs/athletes/$ATHLETE_ID.json
 rm docs/athletes/$ATHLETE_ID.html
--- fetch-athletes.sh
+++ fetch-athletes.sh
@@ -1,3 +1,3 @@
 SOURCE_DIR=$(dirname "$0")
 
-for ATHLETE in $(ls $SOURCE_DIR/docs/athletes | xargs -I{} -n 1 basename {} .json); do $SOURCE_DIR/fetch-athlete.sh $ATHLETE; sleep 2; done
+for ATHLETE in $(ls $SOURCE_DIR/docs/athletes | xargs -I{} -n 1 basename {} .json); do $SOURCE_DIR/fetch-athlete.sh $ATHLETE; done
