7a6204650b79374ef3d9069863c37feb65565ad4
Dave Sowerby-Williams <dave.sowerby@gmail.com>
2024-12-12 06:13:55 +0000

    Correcting echo line for remoinder of the conversion to JSON

--- fetch-athlete.sh
+++ fetch-athlete.sh
@@ -8,7 +8,7 @@ echo Go fetch https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ to docs/ath
 code docs/athletes/$ATHLETE_ID.json
 open https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/
 echo "Tell me when that's done"
-echo "Reminder: javascript:(function(){const links=Array.from(document.querySelectorAll('a[href]'));const pattern=/^(https?:\/\/[^/]+\/([^/]+)\/results\/)$/;const matchingLinks=links.map(link=>link.href.match(pattern)).filter(match=>match).map(match=>match[2]);const uniqueLinks=[...new Set(matchingLinks)].sort();const jsonArray=JSON.stringify(uniqueLinks);document.body.innerHTML=`<pre>${jsonArray}</pre>`;document.body.style.whiteSpace=%27pre-wrap%27;document.body.style.fontFamily=%27monospace%27;})();"
+echo 'Reminder: javascript:(function(){const links=Array.from(document.querySelectorAll('a[href]'));const pattern=/^(https?:\/\/[^/]+\/([^/]+)\/results\/)$/;const matchingLinks=links.map(link=>link.href.match(pattern)).filter(match=>match).map(match=>match[2]);const uniqueLinks=[...new Set(matchingLinks)].sort();const jsonArray=JSON.stringify(uniqueLinks);document.body.innerHTML=`<pre>${jsonArray}</pre>`;document.body.style.whiteSpace=%27pre-wrap%27;document.body.style.fontFamily=%27monospace%27;})();'
 read -p "Press Enter to continue..."
 
 #wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36" https://www.parkrun.org.uk/parkrunner/$ATHLETE_ID/all/ -O docs/athletes/$ATHLETE_ID.html
